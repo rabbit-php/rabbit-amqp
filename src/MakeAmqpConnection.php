ac13d883354071bdbb211df01b50011fc49905dd
Albert <amuluowin@163.com>
2022-02-04 13:12:13 +0800

    opt code

--- src/MakeAmqpConnection.php
+++ src/MakeAmqpConnection.php
@@ -3,26 +3,12 @@ declare(strict_types=1);
 
 namespace Rabbit\Amqp;
 
-use DI\DependencyException;
-use DI\NotFoundException;
 use Rabbit\Pool\BaseManager;
 use Rabbit\Pool\BasePool;
 use Rabbit\Pool\BasePoolProperties;
-use Throwable;
 
-/**
- * Class MakeAmqpConnection
- * @package Rabbit\Amqp
- */
 class MakeAmqpConnection
 {
-    /**
-     * @param string $name
-     * @param array $config
-     * @throws DependencyException
-     * @throws NotFoundException
-     * @throws Throwable
-     */
     public static function addConnection(string $name, array $config = []): void
     {
         /** @var BaseManager $manager */
